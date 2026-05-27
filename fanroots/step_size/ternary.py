701d670af32824adbfbab6a9a7983b9c3f10df22
nate <natemacfadden@gmail.com>
2026-05-27 11:41:08 -0400

    update license info

--- fanroots/step_size/ternary.py
+++ fanroots/step_size/ternary.py
@@ -1,5 +1,6 @@
 # =============================================================================
-#    Copyright (C) 2026  Nate MacFadden for the Liam McAllister Group
+#    Copyright (C) 2026  Nate MacFadden and contributors
+#    Originally developed in the Liam McAllister Group at Cornell University.
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
